79b097c65442d35945869444508ea2b16d3af861
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-27 21:56:31 -0300

    des

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ resource "aws_key_pair" "key_pair" {
 
 resource "local_file" "private_key" {
   content  = tls_private_key.private_key.private_key_pem
-  filename = "${path.module}/my-terraform-key.pem"
+  filename = "my-terraform-key.pem"
 }
 
 resource "aws_security_group" "security_group" {
